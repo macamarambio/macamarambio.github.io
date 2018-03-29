8c85bf373faa7e9ae2e113affda004b81a1c44e2
macamarambio <marambio.macarena@gmail.com>
2018-03-28 21:51:34 -0300

    portafolio

---
+++ app.js
@@ -0,0 +1,7 @@
+function coinConvert(dollar){
+	var soles = dollar* 3.25;
+	var pesosMexicanos = dollar * 18;
+	var pesosChilenos = dollar * 660;
+
+	console.log(soles,pesosMexicanos,pesosChilenos)
+}
